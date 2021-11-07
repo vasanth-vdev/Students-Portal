0798b39190403bfd10ddaaacf33342713619d0f1
Vasanth <vasanthvdev@gmail.com>
2021-11-08 01:42:06 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './assets/css/App.css';
-// import Login from './pages/Login.js'
+import Login from './pages/Login.js';
 import StudentDashboard from './pages/StudentDashboard';
 import TodaySchedule from './pages/TodaySchedule';
 const App = () => {
   return (
     <div>
-      <StudentDashboard>
-        <TodaySchedule />
-      </StudentDashboard>
+      <Router>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <StudentDashboard>
+                <TodaySchedule />
+              </StudentDashboard>
+            }
+          />
+          <Route
+            path='/'
+            element={
+              <StudentDashboard>
+                <TodaySchedule />
+              </StudentDashboard>
+            }
+          />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
