90d5a7740a02a876f8b1397cf09ae9ce8cd23401
vasanth064 <vasanthvdev@gmail.com>
2022-05-02 20:51:55 +0530

    renamed bio to profile

--- src/data/StudentDashboardData.js
+++ src/data/StudentDashboardData.js
@@ -14,101 +14,101 @@ import ProjectsManagement from '../pages/Bio/ProjectsManagement';
 import CertificateManagement from '../pages/Bio/CertificateManagement';
 import StudentsAttendance from '../pages/Academic/StudentsAttendance';
 import CAMarks from '../pages/Academic/CAMarks';
-import {FaUserAlt} from 'react-icons/fa'
+import { FaUserAlt } from 'react-icons/fa';
 // import { BiSitemap } from 'react-icons/bi';
 
 const StudentDashboardData = [
-	{
-		header: {
-			userImage,
-			userName: 'V V Vasanth - 19DX27',
-		},
-		sidebar: {
-			logo,
-			navItems: [
-				{
-					navTitle: 'Dashboard',
-					navIcon: <MdOutlineDashboard />,
-					navURL: '',
-					subMenu: [],
-				},
-				{
-					navTitle: 'Academic',
-					navIcon: <SiMicrosoftacademic />,
-					subMenu: [
-						{
-							title: 'Posted Leave Printout',
-							// icon: <BiSitemap />,
-							URL: 'PostedLeavePrintout',
-							page: <PostedLeavePrintout />,
-						},
-						{
-							title: 'Semester Fee',
-							URL: 'SemesterFeeStructure',
-							page: <SemesterFeeStructure />,
-						},
-						{
-							title: 'Attendance',
-							URL: 'StudentsAttendance',
-							page: <StudentsAttendance />,
-						},
-						{
-							title: 'CA Marks',
-							URL: 'CAMarks',
-							page: <CAMarks />,
-						},
-					],
-				},
-				{
-					navTitle: 'Examination',
-					navIcon: <MdSchool />,
-					subMenu: [
-						{
-							title: 'Test Timetable',
-							URL: 'TestTimetable',
-							page: <TestTimetable />,
-						},
-						{
-							title: 'Seating Arrangement',
-							URL: 'SeatingArrangement',
-							page: <SeatingArrangement />,
-						},
-						{
-							title: 'Exam Timetable',
-							URL: 'ExamTimetable',
-							page: <ExamTimeTable />,
-						},
-						{
-							title: 'Exam Result',
-							URL: 'ExamResult',
-							page: <ExamResult />,
-						},
-					],
-				},
-				{
-					navTitle: 'Bio',
-					navIcon: <FaUserAlt size='2.5rem'/>,
-					subMenu: [
-						{
-							title: 'Update Profile',
-							URL: 'UpdateBio',
-							page: <ProfileUpdate />,
-						},
-						{
-							title: 'Manage Projects',
-							URL: 'ProjectManagement',
-							page: <ProjectsManagement />,
-						},
-						{
-							title: 'Manage Certificates',
-							URL: 'CertificateManagement',
-							page: <CertificateManagement />,
-						},
-					],
-				},
-			],
-		},
-	},
+  {
+    header: {
+      userImage,
+      userName: 'V V Vasanth - 19DX27',
+    },
+    sidebar: {
+      logo,
+      navItems: [
+        {
+          navTitle: 'Dashboard',
+          navIcon: <MdOutlineDashboard />,
+          navURL: '',
+          subMenu: [],
+        },
+        {
+          navTitle: 'Academic',
+          navIcon: <SiMicrosoftacademic />,
+          subMenu: [
+            {
+              title: 'Posted Leave Printout',
+              // icon: <BiSitemap />,
+              URL: 'PostedLeavePrintout',
+              page: <PostedLeavePrintout />,
+            },
+            {
+              title: 'Semester Fee',
+              URL: 'SemesterFeeStructure',
+              page: <SemesterFeeStructure />,
+            },
+            {
+              title: 'Attendance',
+              URL: 'StudentsAttendance',
+              page: <StudentsAttendance />,
+            },
+            {
+              title: 'CA Marks',
+              URL: 'CAMarks',
+              page: <CAMarks />,
+            },
+          ],
+        },
+        {
+          navTitle: 'Examination',
+          navIcon: <MdSchool />,
+          subMenu: [
+            {
+              title: 'Test Timetable',
+              URL: 'TestTimetable',
+              page: <TestTimetable />,
+            },
+            {
+              title: 'Seating Arrangement',
+              URL: 'SeatingArrangement',
+              page: <SeatingArrangement />,
+            },
+            {
+              title: 'Exam Timetable',
+              URL: 'ExamTimetable',
+              page: <ExamTimeTable />,
+            },
+            {
+              title: 'Exam Result',
+              URL: 'ExamResult',
+              page: <ExamResult />,
+            },
+          ],
+        },
+        {
+          navTitle: 'Profile',
+          navIcon: <FaUserAlt size='2.5rem' />,
+          subMenu: [
+            {
+              title: 'Update Profile',
+              URL: 'UpdateProfile',
+              page: <ProfileUpdate />,
+            },
+            {
+              title: 'Manage Projects',
+              URL: 'ManageProjects',
+              page: <ProjectsManagement />,
+            },
+            {
+              title: 'Manage Certificates',
+              URL: 'ManagaeCertificates',
+              page: <CertificateManagement />,
+            },
+          ],
+        },
+      ],
+    },
+  },
 ];
 
 export default StudentDashboardData;
