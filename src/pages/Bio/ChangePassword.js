f229997bf36fc1fe2ad4dccce17ce0d49a1afd46
vasanth064 <vasanthvdev@gmail.com>
2022-05-02 08:29:36 +0530

    added change password

---
+++ src/pages/Bio/ChangePassword.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+import PageContent from "../../components/PageContent";
+
+const ChangePassword = () => {
+    return (
+    <div>
+        <PageHeader text='Change Password'/>
+        <PageContent>
+           Change Password
+        </PageContent>
+    </div>
+    );
+}
+
+export default ChangePassword;
