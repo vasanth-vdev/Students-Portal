6382263d7622c9bd9b9850698a8e08eb3562dd1b
vasanth064 <vasanthvdev@gmail.com>
2022-05-07 16:09:51 +0530

    added todaySchedule

--- src/components/TodayScheduleItem.js
+++ src/components/TodayScheduleItem.js
@@ -2,19 +2,13 @@ import React from 'react';
 import './css/TodayScheduleItem.css';
 
 const TodayScheduleItem = ({ data }) => {
-	return data.length !== 0 ? (
-		data.map((item, index) => (
-			<div className='todayScheduleItem' key={index}>
-				<h1 className='todayScheduleSubject'>{item.subject}</h1>
-				<h1 className='todayScheduleTime'>{item.time}</h1>
-				<h1 className='todayScheduleTimeFacultyIncharge'>
-					{item.facultyIncharge}
-				</h1>
-			</div>
-		))
-	) : (
-		<h1 className='noClassMsg'>No Classes Scheduled Today</h1>
-	);
+  return (
+    <div className='todayScheduleItem'>
+      <h1 className='todayScheduleSubject'>{data.courseTitle}</h1>
+      <h1 className='todayScheduleTime'>{`${data.fromTime} - ${data.toTime}`}</h1>
+      <h1 className='todayScheduleTimeFacultyIncharge'>{data.faculty}</h1>
+    </div>
+  );
 };
 
 export default TodayScheduleItem;
--- src/pages/TodaySchedule.js
+++ src/pages/TodaySchedule.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PageHeader from '../components/PageHeader';
 import PageContent from '../components/PageContent';
 import TodayScheduleData from '../data/TodayScheduleData';
 import TodayScheduleItem from '../components/TodayScheduleItem';
+import { useFirestore } from './../Context/FirestoreContext';
+import { useAuth } from './../Context/AuthContext';
 import './../assets/css/todaySchedule.css';
+import { where } from 'firebase/firestore';
 const TodaySchedule = () => {
   const d = new Date();
   let day = d.getDay();
-
+  const [todaySchedule, setTodaySchedule] = useState([]);
+  const { getData } = useFirestore();
+  const { userData } = useAuth();
+  useEffect(() => {
+    (async () => {
+      const data = await getData(
+        'classTimetable',
+        where('batch', '==', userData.batch) &&
+          where('department', '==', userData.department)
+      );
+      setTodaySchedule(data[0].timeTable);
+    })();
+  }, []);
   return (
     <div className='todaySchedulePage'>
       <PageHeader text={`Today's Schedule`} />
       <PageContent>
         <div className='todayScheduleList'>
-          {TodayScheduleData.map((item, index) => (
-            <TodayScheduleItem data={item[day]} key={index} />
-          ))}
+          {todaySchedule.map((item, index) =>
+            parseInt(item.weekDay) === day ? (
+              <TodayScheduleItem data={item} key={index} />
+            ) : null
+          )}
         </div>
       </PageContent>
     </div>
