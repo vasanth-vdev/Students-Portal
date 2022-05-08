2b2c1803cd87aefc4bf17e0ffa520b3adbbdc998
vasanth064 <vasanthvdev@gmail.com>
2022-05-08 16:02:37 +0530

    updated green button to blue

--- src/components/GreenButton.js
+++ src/components/GreenButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './css/GreenButton.css';
-const GreenButton = ({ children, marginCenter }) => {
+const GreenButton = ({ children, marginCenter, ...props }) => {
   return (
     <button
       type='submit'
+      {...props}
       className={marginCenter ? 'greenButton marginCenter' : 'greenButton'}>
       {children}
     </button>
