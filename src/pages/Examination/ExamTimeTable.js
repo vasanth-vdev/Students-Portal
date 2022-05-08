bcb8a0dfffdd04e45687f023fbbae14ab2da03b6
vasanth064 <vasanthvdev@gmail.com>
2022-05-08 16:05:33 +0530

    now shows data based on user

--- src/pages/Examination/ExamTimeTable.js
+++ src/pages/Examination/ExamTimeTable.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from './../../components/Table';
 import PageHeader from './../../components/PageHeader';
 import PageContent from './../../components/PageContent';
-import ExamTimeTableData from './../../data/Examination/ExamTimeTableData';
+import { useFirestore } from './../../Context/FirestoreContext';
+import { where } from 'firebase/firestore';
+import { useAuth } from '../../Context/AuthContext';
 const ExamTimeTable = () => {
+  const { getData } = useFirestore();
+  const { userData } = useAuth();
+  const [examTimetable, setExamTimetable] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const data = await getData(
+        'examTimetable',
+        where('batch', '==', userData.batch) &&
+          where('department', '==', userData.department) &&
+          where('semester', '==', userData.semester ? userData.semester : '1')
+      );
+      const table = [
+        {
+          titles: ['DATE', 'SESSION', 'COURSE CODE ', ' COURSE TITLE'],
+          data: data[0].timeTable,
+          contents: ['date', 'session', 'courseCode', 'courseTitle'],
+        },
+      ];
+      setExamTimetable(table);
+    })();
+  }, []);
   return (
     <div>
       <PageHeader text='Exam Timetable' />
       <PageContent>
-        <Table data={ExamTimeTableData} />
+        <Table data={examTimetable} />
       </PageContent>
     </div>
   );
